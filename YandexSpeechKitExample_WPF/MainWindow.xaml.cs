5c0c7b7910cca98a7eccf9317dd08d72f15ece85
doomkinn <root@DESKTOP-AIV5DVJ>
2023-05-01 19:59:05 +0300

    версия 1 готова

--- YandexSpeechKitExample_WPF/MainWindow.xaml.cs
+++ YandexSpeechKitExample_WPF/MainWindow.xaml.cs
@@ -9,6 +9,9 @@ using Newtonsoft.Json;
 using System.Text;
 using System.Windows.Controls;
 using System.Net.Http.Headers;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
 
 namespace YandexSpeechKitExample_WPF
 {
@@ -36,6 +39,7 @@ namespace YandexSpeechKitExample_WPF
     public partial class MainWindow : Window
     {
         private Button btnStart;
+        private CancellationTokenSource _cancellationTokenSource;
 
         public MainWindow()
         {
@@ -53,16 +57,29 @@ namespace YandexSpeechKitExample_WPF
                 return;
             }
 
+            _cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = _cancellationTokenSource.Token;
+
+            BtnCancel.IsEnabled = true; // Включаем кнопку отмены
+            BtnCreateAudioFiles.IsEnabled = false;
+            string mode = singleFileRadio.IsChecked.GetValueOrDefault() ? "file" : "folder";
             try
             {
-                await CreateAudioFilesFromTextFiles(directoryPath, selectedVoice.ToString(), selectedEmotion.ToString());
-                MessageBox.Show("Готово!");
+                await CreateAudioFilesFromTextFiles(directoryPath, selectedVoice.ToString(), selectedEmotion.ToString(), cancellationToken, mode);
+                txtStatus.Content = "Готово!";
+            }
+            catch (OperationCanceledException)
+            {
+                txtStatus.Content = "Операция отменена пользователем.";
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Ошибка: {ex.Message}");
+                txtStatus.Content = $"Ошибка: {ex.Message}";
             }
+            BtnCancel.IsEnabled = false; // Отключаем кнопку отмены
+            BtnCreateAudioFiles.IsEnabled = true;
         }
+
 
         private void UpdateProgress(int value, int max)
         {
@@ -72,11 +89,28 @@ namespace YandexSpeechKitExample_WPF
         }
 
 
+        private void RadioButtons_Checked(object sender, RoutedEventArgs e)
+        {
+            if (txtDirectory != null)
+            {
+                txtDirectory.Text = "";
+            }
+        }
+
         private void BtnSelectDirectory_Click(object sender, RoutedEventArgs e)
         {
             using (var dialog = new CommonOpenFileDialog())
             {
-                dialog.IsFolderPicker = true;
+                if (singleFileRadio.IsChecked == true)
+                {
+                    dialog.Filters.Add(new CommonFileDialogFilter("Text Files", ".txt"));
+                    dialog.IsFolderPicker = false;
+                }
+                else
+                {
+                    dialog.IsFolderPicker = true;
+                }
+
                 if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
                 {
                     txtDirectory.Text = dialog.FileName;
@@ -87,11 +121,14 @@ namespace YandexSpeechKitExample_WPF
         private async void BtnStart_Click(object sender, RoutedEventArgs e)
         {
             
-        }       
+        }
 
-        private async Task CreateAudioFilesFromTextFiles(string directoryPath, string selectedVoice, string selectedEmotion)
+        private async Task CreateAudioFilesFromTextFiles(string directoryPath, string selectedVoice, string selectedEmotion, CancellationToken cancellationToken, string mode = "folder")
         {
-            var apiKey = "AQVNz7LJrIiaQHv22qhEWV7Ef0UPBUgUFlH-waHV";
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var apiKey = config.GetSection("YandexApi:ApiKey").Value;
             var apiUrl = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize";
             const int maxTextLength = 4500;
 
@@ -99,52 +136,69 @@ namespace YandexSpeechKitExample_WPF
             {
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Api-Key", apiKey);
 
-                var filePaths = Directory.GetFiles(directoryPath, "*.txt");
+                var filePaths = mode == "folder" ? Directory.GetFiles(directoryPath, "*.txt") : new string[] { directoryPath };
                 progressBar.Minimum = 0;
                 progressBar.Maximum = filePaths.Length;
                 progressBar.Value = 0;
 
                 for (int i = 0; i < filePaths.Length; i++)
                 {
+                    cancellationToken.ThrowIfCancellationRequested();
+
                     string text = await File.ReadAllTextAsync(filePaths[i]);
                     string outputFilePath = Path.ChangeExtension(filePaths[i], ".mp3");
 
-                    var textChunks = new List<string>();
-                    for (int j = 0; j < text.Length; j += maxTextLength)
-                    {
-                        textChunks.Add(text.Substring(j, Math.Min(maxTextLength, text.Length - j)));
-                    }
+                    var paragraphs = text.Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    progressBar.Minimum = 0;
+                    progressBar.Maximum = paragraphs.Length;
+                    progressBar.Value = 0;
 
                     using (var outputFile = File.Create(outputFilePath))
                     {
-                        foreach (var chunk in textChunks)
+                        foreach (var paragraph in paragraphs)
                         {
-                            var content = new FormUrlEncodedContent(new Dictionary<string, string>
-                            {
-                                {"text", chunk},
-                                {"lang", "ru-RU"},
-                                {"voice", selectedVoice},
-                                {"emotion", selectedEmotion},
-                                {"format", "mp3" },
-                                {"speed", "1.25" },
-                                {"sampleRateHertz", "48000" }                                
-                            });
+                            cancellationToken.ThrowIfCancellationRequested();
 
-                            using (var response = await httpClient.PostAsync(apiUrl, content))
+                            var textChunks = new List<string>();
+                            for (int j = 0; j < paragraph.Length; j += maxTextLength)
                             {
-                                if (response.IsSuccessStatusCode)
+                                textChunks.Add(paragraph.Substring(j, Math.Min(maxTextLength, paragraph.Length - j)));
+                            }
+
+                            foreach (var chunk in textChunks)
+                            {
+                                cancellationToken.ThrowIfCancellationRequested();
+
+                                var content = new FormUrlEncodedContent(new Dictionary<string, string>
                                 {
-                                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                                    {"text", chunk},
+                                    {"lang", "ru-RU"},
+                                    {"voice", selectedVoice},
+                                    {"emotion", selectedEmotion},
+                                    {"format", "mp3" },
+                                    {"speed", "1.2" },
+                                    {"sampleRateHertz", "48000" }
+                                });
+
+                                using (var response = await httpClient.PostAsync(apiUrl, content, cancellationToken))
+                                {
+                                    if (response.IsSuccessStatusCode)
                                     {
-                                        await responseStream.CopyToAsync(outputFile);
+                                        using (var responseStream = await response.Content.ReadAsStreamAsync())
+                                        {
+                                            await responseStream.CopyToAsync(outputFile);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        txtStatus.Content = $"Ошибка: {response.StatusCode}, {await response.Content.ReadAsStringAsync()}";
+                                        break;
                                     }
                                 }
-                                else
-                                {
-                                    txtStatus.Content = $"Ошибка: {response.StatusCode}, {await response.Content.ReadAsStringAsync()}";
-                                    break;
-                                }
                             }
+
+                            progressBar.Value++;
+                            progressLabel.Content = $"{((progressBar.Value / progressBar.Maximum) * 100):F1}%";
                         }
                     }
 
@@ -156,5 +210,13 @@ namespace YandexSpeechKitExample_WPF
             txtStatus.Content = "Завершено!";
         }
 
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+            }
+        }
     }
 }
